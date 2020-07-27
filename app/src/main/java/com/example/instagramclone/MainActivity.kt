c3c71b038f0bac311b891cbd1b30d1d8d25abfd9
Shikha Shah <shahshikha119@gmail.com>
2020-07-27 12:29:28 -0700

    profile fragment ui
    added some ui for profile fragment

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.example.instagramclone
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.instagramclone.Fragments.HomeFragment
 import com.example.instagramclone.Fragments.NotificationsFragment
 import com.example.instagramclone.Fragments.ProfileFragment
