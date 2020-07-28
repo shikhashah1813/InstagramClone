eca5ea02aecf650e5f8d01306061f15136ce9b1f
Shikha Shah <shahshikha119@gmail.com>
2020-07-28 12:01:38 -0700

    added sign up and login activity

---
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.example.instagramclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        button_login.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+    }
+}
